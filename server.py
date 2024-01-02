be0f94fcc83a844d14416282689279f9145e877f
JeansAthiwat <athiwat5146@gmail.com>
2024-01-02 04:13:55 -0500

    linted yay

--- server.py
+++ server.py
@@ -1,32 +1,28 @@
+"""
+API server endpoint for the web app
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
-app = Flask("__name__")
+app = Flask(__name__)
 
 @app.route("/")
 def home():
-    '''Render the HTML index home page'''
+    """Render the HTML index home page."""
     return render_template("index.html")
 
 @app.route("/emotionDetector")
-def emotionDetector():
+def emotion_detector_route():
+    """Endpoint to analyze emotion."""
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
-
-    anger = response['anger']
-    disgust = response['disgust']
-    fear = response['fear']
-    joy = response['joy']
-    sadness = response['sadness']
-    dominant_emotion = response['dominant_emotion']
-
-    if dominant_emotion is None:
+    if response is None:
         return "Invalid text! Please try again!"
-    else:
-        return f"""For the given statement, the system response is: 
-        'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 
-        'joy': {joy},'sadness': {sadness}.The dominant emotion is {dominant_emotion}."""
-
+    emotion_keys = ['anger', 'disgust', 'fear', 'joy', 'sadness']
+    emotion_values = ', '.join(f"'{key}': {response.get(key, 0)}" for key in emotion_keys)
+    dominant_emotion = response.get('dominant_emotion', 'N/A')
+    return f"""For the given statement, the system response is:
+    {emotion_values}. The dominant emotion is {dominant_emotion}."""
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
