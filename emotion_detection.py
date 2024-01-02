e9de9b51c10b33ac9ea4fcdb92760d78bb9b0a91
JeansAthiwat <athiwat5146@gmail.com>
2024-01-02 02:59:11 -0500

    format emtion output

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,8 +1,23 @@
 import requests
 import json
 
-def emotion_detector(text_to_analyze):
+def emotion_predictor(formatted_response):
+    '''Returns :
+    {
+    'anger': anger_score,
+    'disgust': disgust_score,
+    'fear': fear_score,
+    'joy': joy_score,
+    'sadness': sadness_score,
+    'dominant_emotion': '<name of the dominant emotion>'
+    }
+    '''
+    emotions = formatted_response["emotionPredictions"][0]["emotion"]
+    dominant_emotion = max(emotions, key=emotions.get)
+    emotions["dominant_emotion"] = dominant_emotion
+    return emotions
 
+def emotion_detector(text_to_analyze):
     URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     HEADERS = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
@@ -11,8 +26,8 @@ def emotion_detector(text_to_analyze):
     formatted_response = json.loads(response.text)
 
     if response.status_code == 200:
-        return formatted_response
-
+        emotions = emotion_predictor(formatted_response)
+        return emotions
     else:
         return {"message" : "sum ting wong"}
 
